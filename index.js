6d3b33de51de1b1a2b8506dbf5e475bc75b6a132
BinhGiauTen <binhpro50@gmail.com>
2024-05-03 15:52:39 +0700

    test 7

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const {add,minus} = require('./functions')
 
 console.log('Hello world again');
 console.log('Today is a beautiful day')
-console.log('Test 6')
+console.log('Test 7')
 var a = 2
 var b = 9
 console.log(util.format("Add: %d+%d=%d", a, b, add(a,b)))
